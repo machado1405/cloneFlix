85a1ab964ac33d8236bb24fb401acc9a63c1db26
Matheus Da Silva Machado <agromsmachado@gmail.com>
2023-08-23 19:31:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Projeto de uma biblioteca de filmes e séries.
+Utilizando React/Vite, react-hook form, material UI.
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <p>Primeiro commit</p>
+    </>
+  );
+}
+
+export default App;
